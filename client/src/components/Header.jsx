d337e5e9a677b051a8747ad58951a038b10dd1c6
Youngjae <2015fromjeki@gmail.com>
2024-12-08 22:16:12 +1100

    Added Music Funciton

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useState, useRef } from "react";
 import { FaPlusSquare } from "react-icons/fa";
 import { IoMdHome } from "react-icons/io";
-
+import { MdMusicNote } from "react-icons/md";
+import { MdMusicOff } from "react-icons/md";
 import { Link } from "react-router-dom";
+// Music Player Setting
+
 const Header = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
   return (
     <div className="lg:px-100 px-[10%] py-4">
       <div className="flex items-center justify-between">
@@ -12,7 +27,7 @@ const Header = () => {
             Quotable
           </h1>
         </Link>
-        <div className="flex gap-x-4 p-2 duration-300">
+        <div className="flex items-center gap-x-4 p-2 duration-300">
           <Link to={"/"}>
             <div className="rounded-xl p-2 duration-300 hover:bg-yellow-200">
               <IoMdHome className="rounded-xl text-2xl hover:cursor-pointer" />
@@ -23,6 +38,21 @@ const Header = () => {
               <FaPlusSquare className="rounded-xl text-2xl hover:cursor-pointer" />
             </div>
           </Link>
+          <div>
+            <button onClick={togglePlay}>
+              {isPlaying ? (
+                <MdMusicOff className="rounded-xl text-2xl hover:cursor-pointer" />
+              ) : (
+                <MdMusicNote className="rounded-xl text-2xl hover:cursor-pointer" />
+              )}
+            </button>
+            <audio
+              ref={audioRef}
+              src="../../public/music.mp3"
+              type="audio/mp3"
+              autoPlay
+            />
+          </div>
         </div>
       </div>
     </div>
